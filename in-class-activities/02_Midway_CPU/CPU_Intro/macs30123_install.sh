4e774c8e157680da9e2bcd3f3942b2060a172150
jonclindaniel <jclindaniel@uchicago.edu>
2025-03-26 12:01:38 -0500

    Add Week 2 Materials

---
+++ in-class-activities/02_Midway_CPU/CPU_Intro/macs30123_install.sh
@@ -0,0 +1,4 @@
+# Install packages we'll be using on Midway 3
+module load python/anaconda-2022.05 mpich/3.2.1 cuda/11.7
+pip install --user numba==0.57.1 numpy==1.22.4 mpi4py-mpich==3.1.5 \
+                   pyopencl==2024.1 pycuda==2024.1 rasterio==1.3.9
